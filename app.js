3197fb7c3a6fb740375d37b02d7890200c326760
Andrii-Velhan <velganaa@gmail.com>
2020-12-03 21:29:52 +0200

    add styles, html, and app.js

---
+++ app.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const refs = {
+    controls: document.querySelector('#tabs-1 [data-controls]'),
+    panes: document.querySelector('#tabs-1 [data-panes]')
+};
+
+refs.controls.addEventListener('click', event => {
+    event.preventDefault();
+
+    if (event.target.nodeName !== 'A') {
+        console.log('Кликнули не туда');
+        return
+    }
+
+    const currentActiveControlItem = refs.controls.querySelector(
+        '.controls__item--active'
+    );
+
+    if (currentActiveControlItem) {
+        currentActiveControlItem.classList.remove('controls__item--active');
+
+        const paneId = currentActiveControlItem.getAttribute('href').slice(1);
+        const pane = refs.panes.querySelector(`#${paneId}`);
+        // console.log(paneId);
+        pane.classList.remove('pane--active');
+    }
+
+    const controlItem = event.target;
+    controlItem.classList.add('controls__item--active');
+
+
+    const paneId = controlItem.getAttribute('href').slice(1);
+    // console.log(paneId);
+
+    const pane = refs.panes.querySelector(`#${paneId}`);
+    pane.classList.add('pane--active');
+     // console.log(pane);
+})
